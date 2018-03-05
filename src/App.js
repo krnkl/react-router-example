369c1e0f1999a991d6d3a67363976a9873a73b5e
Nikolai Krapivchenko <krapivchenkon@gmail.com>
2018-03-05 10:17:19 +0100

    Fixed linter errors

---
+++ src/App.js
@@ -0,0 +1,63 @@
+
+import React from "react";
+import PropTypes from "prop-types";
+import {
+  BrowserRouter as Router,
+  Route,
+  NavLink as Link,
+  Switch
+} from "react-router-dom";
+import "./App.css";
+
+const isActiveFunc = (match) => (match);
+const Page = ({ children }) => (<h1 className="header">{children}</h1>);
+
+Page.propTypes = { children: PropTypes.object.isRequired };
+
+const Links = () => (
+  <nav>
+    <Link exact activeClassName="active" to="/">
+      Home
+    </Link>
+    {"  "}
+    <Link to={{ pathname: "/about" }}>About</Link>
+    {"  "}
+    <Link isActive={isActiveFunc} activeClassName="active" to="/contact">
+      Contact
+    </Link>
+    {"  "}
+    <Link to="/components?id=component-one">Component1</Link>
+  </nav>
+);
+
+
+Links.propTypes = { location: PropTypes.element.isRequired };
+
+const Comp = ({ location }) => (
+    <div className="content">
+      <h2>Component {}</h2>
+      <p>{JSON.stringify(location)}</p>
+      <p>{new URLSearchParams(location.search).get("id")}</p>
+    </div>
+  );
+
+Comp.propTypes = { location: PropTypes.element.isRequired };
+
+const App = () => (
+  <Router>
+    <div>
+      <Links />
+      <Switch>
+        <Route path="/components" component={Comp} />
+        <Route
+          exact
+          path="/:page?"
+          render={({ match }) => <Page>{match.params.page || "home"}</Page>}
+        />
+        <Route render={() => <h2>PageNotFound</h2>} />
+      </Switch>
+    </div>
+  </Router>
+);
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,65 +1,6 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Route,
-  NavLink as Link,
-  Switch
-} from "react-router-dom";
-import { render } from "react-dom";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-const styles = {
-  fontFamily: "anonymous-pro",
-  textAlign: "center"
-};
-const isActiveFunc = (match, location) => {
-  console.log(match, location);
-  return match;
-};
-const Page = ({ children }) => {
-  console.log(`${children} match:`);
-  return <h1>{children}</h1>;
-};
+ReactDOM.render(<App />, document.getElementById("root"));
 
-const Links = () => (
-  <nav>
-    <Link exact activeClassName="active" to="/">
-      Home
-    </Link>
-    {"  "}
-    <Link to={{ pathname: "/about" }}>About</Link>
-    {"  "}
-    <Link isActive={isActiveFunc} activeClassName="active" to="/contact">
-      Contact
-    </Link>
-    {"  "}
-    <Link to="/components?id=component-one">Component1</Link>
-  </nav>
-);
-const Comp = ({ match, location }) => {
-  console.log("Component match", match);
-  return (
-    <div>
-      <h2>Component {}</h2>
-      <p>{JSON.stringify(location)}</p>
-      <p>{new URLSearchParams(location.search).get("id")}</p>
-    </div>
-  );
-};
-const App = () => (
-  <Router>
-    <div>
-      <Links />
-      <Switch>
-        <Route
-          exact
-          path="/:page(!components)?"
-          render={({ match }) => <Page>{match.params.page || "home"}</Page>}
-        />
-        <Route path="/components" component={Comp} />
-        <Route render={() => <h2>PageNotFound</h2>} />
-      </Switch>
-    </div>
-  </Router>
-);
-
-render(<App />, document.getElementById("root"));
